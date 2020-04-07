8d01a4195bbd171e8b03f77ab10bf20961a247df
nicomontale <roxas.nm@gmail.com>
2020-04-07 16:51:56 +0200

    change

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,27 +6,27 @@ class LandingPage extends Component {
         return (
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
-                    <img src='https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png'
-                        alt="avatar"
-                        className="avatar-image" />
-                    <div className="banner-text">
-                        <h1>Full Stack Developer</h1>
+                    <Cell col={12}>
+                        <img src='https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png'
+                            alt="avatar"
+                            className="avatar-image" />
+                        <div className="banner-text">
+                            <h1>Full Stack Developer</h1>
 
-                        <hr />
-                        <p>JAVA   |    SQL    |    JAVASCRIPT    |    REACT    |    HTML    |    CSS    |    PHP    |    DATABASE </p>
-                        <div className="social-links">
-                            {/* Linkedin */}
-                            <a href="https://www.linkedin.com/in/nicola-montaleone-7a3202194/" rel="noopener noreferrer" target="_black">
-                                <i className="fa fa-linkedin-square" aria-hidden="true" />
-                            </a>
-                            {/* GitHub */}
-                            <a href="https://github.com/nicomontale" rel="noopener noreferrer" target="_black">
-                                <i className="fa fa-github-square" aria-hidden="true" />
-                            </a>
+                            <hr />
+                            <p>JAVA   |    SQL   |   REACT     </p>
+                            <div className="social-links">
+                                {/* Linkedin */}
+                                <a href="https://www.linkedin.com/in/nicola-montaleone-7a3202194/" rel="noopener noreferrer" target="_black">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+                                {/* GitHub */}
+                                <a href="https://github.com/nicomontale" rel="noopener noreferrer" target="_black">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                            </div>
                         </div>
-                    </div>
-
+                    </Cell>
                 </Grid>
             </div>
         )
--- src/components/project.js
+++ src/components/project.js
@@ -1,206 +1,122 @@
-import React, {Component} from 'react';
-import {Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, CardActions, Button, CardMenu, IconButton} from 'react-mdl';
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 
 class Project extends Component {
     constructor(props) {
         super(props);
-        this.state= {activeTab :0}
+        this.state = { activeTab: 0 }
     }
-toggleCategories() {
-    if(this.state.activeTab===0) {
-        return(
-        <div className="projects-grid">
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <div className="projects-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover' }}></CardTitle>
+                        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.
+                        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,
+                        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
         per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
+                        <CardActions borders>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
 
 
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover' }}></CardTitle>
+                        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.
+                        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,
+                        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
         per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
+                        <CardActions borders>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
 
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/como-funciona-reactjs.png) center / cover' }}></CardTitle>
+                        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.
+                        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,
+                        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
         per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
-        
-        </div>
+                        <CardActions borders>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
 
-        
-           
+                </div>
 
-            
 
-            
-        )
-    } else if(this.state.activeTab===1) {
-        return(
-            <div className="projects-grid">
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://icon-library.net/images/java-icon-image/java-icon-image-0.jpg) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
+
+
+
+
+
+            )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div className="projects-grid">
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://icon-library.net/images/java-icon-image/java-icon-image-0.jpg) center / cover' }}></CardTitle>
+                        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.
+                        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo,
+                        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
         per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
+                        <CardActions borders>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                            <Button colored>GitHub</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
 
 
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://icon-library.net/images/java-icon-image/java-icon-image-0.jpg) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
-        per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
-
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://icon-library.net/images/java-icon-image/java-icon-image-0.jpg) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
-        per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
-        
-        </div>
-        )
-    } else if(this.state.activeTab===2) {
-        return(
-            <div className="projects-grid">
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
-        per preparare un testo campione.  </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
 
 
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
-        per preparare un testo campione.  </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
-
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-        <CardTitle style={{color:'#fff', height: '176px', background:'url(https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png) center / cover'}}></CardTitle>
-        <CardText>Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. 
-        Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, 
-        quando un anonimo tipografo prese una cassetta di caratteri e li assemblò 
-        per preparare un testo campione. </CardText>
-        <CardActions borders>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        <Button colored>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color:'#fff'}}>
-        <IconButton name="share"/>
-        </CardMenu>
-        </Card>
-        
-        </div>
-        )
+                </div>
+            )
+        }
     }
-}
-   
+
     render() {
-        return(
-        <div className="category-tabs">
-        <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab : tabId}) } ripple >
-        <Tab>React</Tab>
-        <Tab>Java</Tab>
-        <Tab>HTML</Tab>
-        
-        
-        </Tabs>
-        <section>
-        <Grid>
-        <Cell col={12}>
-        <div className="content">{this.toggleCategories()}</div>
-        </Cell>
-        </Grid>
-        
-        </section>
-        
-        
-        </div>
+        return (
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple >
+                    <Tab>React</Tab>
+                    <Tab>Java</Tab>
+
+
+
+                </Tabs>
+                <section>
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+
+                </section>
+
+
+            </div>
         )
     }
 }
