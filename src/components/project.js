3ea7e2bed27e163b3c15876dc6f946224ee253c5
nicomontale <roxas.nm@gmail.com>
2020-04-13 15:28:52 +0200

    a

--- src/components/project.js
+++ src/components/project.js
@@ -52,7 +52,10 @@ class Project extends Component {
                         </CardActions>
                         <CardMenu style={{ color: '#fff' }}>
                         
-                        <IconButton href="#" data-toggle="modal" data-target="#testModal" name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} onClick={this.handleShowDialogNTT} raised ripple/>
+                       
+                        
+                        <a href="#" data-toggle="modal" data-target="#testModal"> <IconButton  name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} onClick={this.handleShowDialogNTT} raised ripple/></a>
+                        
                        
                           
                            
