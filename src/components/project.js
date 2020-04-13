c25cdb88ac20c376276e2d803872c846a242be50
nicomontale <roxas.nm@gmail.com>
2020-04-13 16:16:07 +0200

    press

--- src/components/project.js
+++ src/components/project.js
@@ -52,7 +52,7 @@ class Project extends Component {
                         </CardActions>
                         <CardMenu style={{ color: '#fff' }}>
                        
-                        <IconButton  className="clickable" name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} onClick={this.handleShowDialogNTT} raised ripple/>
+                        <IconButton  className="clickable" name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} onPress={this.handleShowDialogNTT} raised ripple/>
                       
 
                           
