a98715d61c2a2ebe09907fb48d74ea0eccb11086
nicomontale <roxas.nm@gmail.com>
2020-04-13 20:41:18 +0200

    cursor

--- src/components/project.js
+++ src/components/project.js
@@ -52,7 +52,7 @@ class Project extends Component {
                         </CardActions>
                         <CardMenu style={{ color: '#fff' }}>
                        
-                       <Tappable onTap={this.handleShowDialogNTT}><IconButton  className="clickable" name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} /></Tappable> 
+                       <Tappable onTap={this.handleShowDialogNTT} style={{cursor:'pointer'}}><IconButton  className="clickable" name ="photo" style={{color: '#da7c18',  width: '58px', height: '58px', minWidth:'initial', cursor:'pointer'}} /></Tappable> 
                       
 
                           
