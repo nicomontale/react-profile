8a85cfc7b041b74e0e4266a585816d5ab7f70d99
nicomontale <roxas.nm@gmail.com>
2020-07-24 14:16:52 +0200

    delete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import {Grid, Cell} from 'react-mdl';
 class Login extends Component {
 	constructor(props) {
